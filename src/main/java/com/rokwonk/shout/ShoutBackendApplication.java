2304b07e931772a45adaa06afdd61283e6038637
RokwonK <wonrok97@naver.com>
2023-07-09 16:44:13 +0900

    ✨ BassTime 및 Auditing 추가

--- src/main/java/com/rokwonk/shout/ShoutBackendApplication.java
+++ src/main/java/com/rokwonk/shout/ShoutBackendApplication.java
@@ -2,7 +2,9 @@ package com.rokwonk.shout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ShoutBackendApplication {
 
